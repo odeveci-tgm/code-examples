b6169aca9d38a5e89b2bc70e13969408ba077755
Michael Borko <michael@borko.at>
2016-02-19 14:57:47 +0100

    Added RMI Example

---
+++ java/rmiCommandPattern/src/server/Server.java
@@ -0,0 +1,38 @@
+package server;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import remoteService.DoSomethingService;
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			ServerService uRemoteObject = new ServerService();
+			DoSomethingService stub = (DoSomethingService) UnicastRemoteObject.exportObject(uRemoteObject, 0);
+			Registry registry = LocateRegistry.createRegistry(1234);
+			registry.rebind("Service", stub);
+            System.out.println("Service bound");
+			
+		} catch (RemoteException re) {
+			System.err.println("Service already bound?" + " Check your RMI-Registry settings!");
+			System.exit(1);
+		} catch (Exception e) {
+			System.err.println("Service exception:");
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+	}
+
+}
